390e7a0e72b950d50033d648d0eea5c834100117
Nguyen Hoang Phuc <81422862+Phuc262002@users.noreply.github.com>
2024-05-07 16:25:34 +0700

    Revert "refactor: Update set-page-layout.global.ts middleware to handle additional routes"
    This reverts commit 772a5814fad43761d00625440f787d5f5fa56856.

--- layouts/none/default.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <div class="min-h-[90vh] container mx-auto">
-            <slot />
-        </div>
-    </div>
-</template>
--- middleware/set-page-layout.global.ts
+++ middleware/set-page-layout.global.ts
@@ -1,12 +1,5 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  let layout = '';
-  if (to.fullPath.startsWith('/admin')) {
-    layout = 'admin-default'
-  } else if (to.fullPath.startsWith('/login') || to.fullPath.startsWith('/register')) {
-    layout = 'none-default'
-  } else {
-    layout = 'client-default'
-  }
-
-  to.meta.layout = layout as any
+export default defineNuxtRouteMiddleware((to,from) => {
+  const layout = to.fullPath.startsWith('/admin') ? 'admin-default' : 'client-default'
+  to.meta.layout = layout
 })
+  
