d48622ec4013d365d62270effb52df06e5723a7a
Nguyen Hoang Phuc <81422862+Phuc262002@users.noreply.github.com>
2024-05-14 18:58:36 +0700

    chore: Remove CommonBreadcrum component from blog pages

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,8 +1,5 @@
 <template>
     <div class="md:px-20 px-8 md:container md:mx-auto md:py-10 py-5">
-        <div class="pb-5">
-            <CommonBreadcrum />
-        </div>
         <div class="pb-12">
             <BlogBanner />
         </div>
