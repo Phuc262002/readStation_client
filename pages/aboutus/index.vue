44caad8dad7d16f9fe0ba2bc9d7c7d24c9540c7e
Nhan2095 <nhanntps26577@fpt.edu.vn>
2024-05-15 11:40:56 +0700

    update

---
+++ pages/aboutus/index.vue
@@ -0,0 +1,156 @@
+<template>
+  <div>
+    <div class="flex items-center justify-center min-h-screen relative">
+      <!-- Hình ảnh -->
+      <div class="absolute inset-0 overflow-hidden p-8">
+        <img
+          src="../../assets/images/aboutus.svg"
+          alt=""
+          class="object-cover w-full h-full rounded-3xl"
+        />
+      </div>
+
+      <!-- Phần nội dung văn bản -->
+      <div
+        class="w-1/2 p-8 absolute top-1/2 transform -translate-y-1/2 left-30"
+      >
+        <h1 class="text-4xl font-bold pb-4">Thuê sách không giới hạn</h1>
+        <ul class="text-lg pb-6 space-y-2">
+          <li>Thuê sách không giới hạn ngày trả</li>
+          <li>Tặng 60 ngày thuê cho thành viên mới</li>
+          <li>Bảo lưu vĩnh viễn khi không thuê sách</li>
+          <li>Không phát sinh phụ phí</li>
+        </ul>
+        <button
+          class="bg-blue-600 text-white px-6 py-3 rounded-md shadow-md hover:bg-blue-700"
+        >
+          Tham gia ngay
+        </button>
+      </div>
+    </div>
+
+    <div class="bg-white p-16">
+      <div
+        class="mx-auto flex flex-col md:flex-row items-center md:items-start text-center md:text-left space-x-8"
+      >
+        <div class="flex-shrink-0 md:w-1/2 lg:w-1/3">
+          <img
+            src="https://via.placeholder.com/800x600"
+            alt="Car Image"
+            class="w-full"
+          />
+        </div>
+        <div class="p-4">
+          <div class="text-3xl font-bold pb-4">Tổng quan về chúng tôi</div>
+          <div class="text-lg pb-4">
+            Read Station là điểm đến lý tưởng cho những người yêu sách muốn tận
+            hưởng sự đa dạng của thế giới văn học mà không cần phải chi tiêu lớn
+            cho việc mua sách. Khách hàng có thể đăng ký thành viên và chọn lựa
+            từ một kho sách phong phú, bao gồm các thể loại từ tiểu thuyết đến
+            sách khoa học, lịch sử và tự truyện. Giá cả cho thuê cũng đa dạng,
+            và cửa hàng thường cung cấp dịch vụ khách hàng tốt để hỗ trợ khách
+            hàng trong việc chọn sách và đáp ứng nhu cầu đọc sách.
+          </div>
+          <div class="space-x-4 py-4">
+            <a
+              href="#"
+              class="bg-[#f3f3f3] text-black text-base px-8 py-3 rounded-lg hover:bg-[#40d7ec] hover:text-white hover:shadow-lg"
+              >Sản phẩm</a
+            >
+            <a
+              href="#"
+              class="bg-[#f3f3f3] text-black text-base px-8 py-3 rounded-lg hover:bg-[#40d7ec] hover:text-white hover:shadow-lg"
+              >Sản phẩm</a
+            >
+            <a
+              href="#"
+              class="bg-[#f3f3f3] text-black text-base px-8 py-3 rounded-lg hover:bg-[#40d7ec] hover:text-white hover:shadow-lg"
+              >Sản phẩm</a
+            >
+          </div>
+          <div class="text-lg py-4">
+            Read Station thường xuyên tổ chức các sự kiện văn hóa như các buổi
+            đọc sách, câu chuyện kể, hoặc các hội thảo về văn hóa đọc. Điều này
+            tạo ra một không gian gặp gỡ, giao lưu và chia sẻ đam mê sách giữa
+            các độc giả. Không chỉ là nơi mượn sách, Read Station còn là điểm
+            đến tương tác xã hội và văn hóa quan trọng đối với cộng đồng đọc
+            sách.
+          </div>
+          <div class="text-lg py-4">
+            <a
+              href="#"
+              class="text-base px-8 py-3 rounded-lg bg-[#40d7ec] text-black hover:shadow-lg"
+              >Tìm hiểu thêm</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="bg-gray-100 p-8">
+      <div class="max-w-4xl mx-auto text-center">
+        <h2 class="text-3xl font-bold mb-4">
+          The Feature Of Carlio Engine Oil That You Will Benefit From
+        </h2>
+        <div class="flex justify-center space-x-4">
+          <div class="bg-white border rounded-lg p-6 w-1/3">
+            <h3 class="font-bold">High Quality Product</h3>
+          </div>
+          <div class="bg-white border rounded-lg p-6 w-1/3">
+            <h3 class="font-bold">Cleaning of deposits</h3>
+          </div>
+          <div class="bg-white border rounded-lg p-6 w-1/3">
+            <h3 class="font-bold">Engine protection</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="flex p-4">
+      <div class="w-1/2">
+        <img
+          src="https://via.placeholder.com/800x600"
+          alt="Car Image"
+          class="w-full"
+        />
+      </div>
+      <div class="w-1/2 ">
+        <section class=" p-8">
+          <div class="max-w-4xl mx-auto">
+            <h2 class="text-3xl font-bold mb-4 text-center">Câu hỏi thường gặp</h2>
+            <div class="space-y-4">
+              <div class="bg-white p-4 rounded-lg">
+                <h3 class="font-bold text-lg">
+                  Trứng có trước hay gà có trước ???
+                </h3>
+                <p class="mt-2">
+                  Nào cũng được. Có để ăn là được
+                </p>
+              </div>
+              <div class="bg-white p-4 rounded-lg">
+                <h3 class="font-bold text-lg">
+                  Trứng có trước hay gà có trước ???
+                </h3>
+                <p class="mt-2">
+                  Nào cũng được. Có để ăn là được
+                </p>
+              </div>
+              <div class="bg-white p-4 rounded-lg">
+                <h3 class="font-bold text-lg">
+                  Trứng có trước hay gà có trước ???
+                </h3>
+                <p class="mt-2">
+                  Nào cũng được. Có để ăn là được
+                </p>
+              </div>
+             
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
