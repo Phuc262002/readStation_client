9c732aef808f8b13ef8e6c8569a9053b565f27ee
Nguyen Hoang Phuc <81422862+Phuc262002@users.noreply.github.com>
2024-05-16 22:09:38 +0700

    refactor: Update sidebar and footer styles

--- layouts/client/default.vue
+++ layouts/client/default.vue
@@ -2,7 +2,7 @@
     <div>
         <IncClientAppHeader/>
         <div class="min-h-[90vh] bg-[#f8f7fa] dark:bg-[#0f172a] pt-24">
-            <div class="md:px-20 px-8 md:container md:mx-auto md:py-10 py-5">
+            <div>
                 <slot />
             </div>
         </div>
