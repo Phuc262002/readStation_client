f783d66fb0063b7010762326a79edb4ec1decc24
Tên của bạn <email@ca.bn>
2024-05-16 20:08:10 +0700

    update

--- components/Inc/Client/AppFooter.vue
+++ components/Inc/Client/AppFooter.vue
@@ -12,12 +12,16 @@
                     <div class="w-1/4 flex flex-col gap-3">
                         <h1 class="text-xl font-medium uppercase text-white">ReadSation</h1>
                         <ul class="flex flex-col gap-2">
-                            <li class="text-white text-base hover:-translate-y-1 transition-transform hover:text-blue-500">
+                            <li
+                                class="text-white text-base hover:-translate-y-1 transition-transform hover:text-blue-500">
                                 Terms of service
                             </li>
-                            <li class="text-white text-base hover:-translate-y-1 transition-transform hover:text-blue-500">
-                                Privacy Policy
-                            </li>
+                            <Nuxt-link to="/privacy-policy">
+                                <li
+                                    class="text-white text-base hover:-translate-y-1 transition-transform hover:text-blue-500">
+                                    Privacy Policy
+                                </li>
+                            </Nuxt-link>
                         </ul>
                     </div>
                     <!-- Phương Thức Thanh Toán -->
@@ -27,11 +31,11 @@
                             <img class="w-14 rounded-md hover:-translate-y-1 hover:scale-110 duration-300"
                                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Old_Visa_Logo.svg/1280px-Old_Visa_Logo.svg.png"
                                 alt="" />
-        
+
                             <img class="w-14 rounded-md hover:-translate-y-1 hover:scale-110 duration-300"
                                 src="https://static-00.iconduck.com/assets.00/mastercard-icon-2048x1313-j69pg4er.png"
                                 alt="" />
-        
+
                             <img class="w-16 rounded-md hover:-translate-y-1 hover:scale-110 duration-300"
                                 src="https://static.ybox.vn/2021/9/4/1631756121713-1631085786958-Thi%E1%BA%BFt%20k%E1%BA%BF%20kh%C3%B4ng%20t%C3%AAn%20-%202021-09-08T002253.248.png"
                                 alt="" />
