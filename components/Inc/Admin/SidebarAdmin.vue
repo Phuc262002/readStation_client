049e32c35bb31366e461facbf15c322117f4fd8c
khuong03 <khuongttaps26697@fpt.edu.vn>
2024-05-16 21:16:27 +0700

    refactor: Update category page components and styles

--- components/Inc/Admin/SidebarAdmin.vue
+++ components/Inc/Admin/SidebarAdmin.vue
@@ -1,53 +1,152 @@
 <template>
-   <a-layout-sider v-model:collapsed="props.collapsed" :trigger="null" collapsible class="!fixed h-full !bg-white dark:!bg-[#132337] !border-r border-r-[#e2e8f0]">
-            <div class="bg-red-400 h-8 m-4" />
-            <a-menu
-                v-model:selectedKeys="selectedKeys"
-                :theme="$colorMode.value === 'dark' ? 'dark' : 'light'"
-                mode="inline"
-                class="!border-0 dark:!bg-[#132337] dark:!text-white"
-            >
-                <a-menu-item key="1">
-                    <pie-chart-outlined />
-                    <span>Option 1</span>
-                </a-menu-item>
-                <a-menu-item key="2">
+  <a-layout-sider
+    v-model:collapsed="props.collapsed"
+    :trigger="null"
+    collapsible
+    class="!fixed h-full !bg-white dark:!bg-[#132337] !border-r border-r-[#e2e8f0]"
+  >
+    <div class="bg-red-400 h-8 m-4" />
+    <a-menu
+      v-model:selectedKeys="selectedKeys"
+      :theme="$colorMode.value === 'dark' ? 'dark' : 'light'"
+      mode="inline"
+      class="!border-0 dark:!bg-[#6c819b] dark:!text-white"
+    >
+      <a-menu-item key="1">
+        <pie-chart-outlined />
+        <span>Dashboards</span>
+      </a-menu-item>
+      <!-- <a-menu-item key="2">
                     <desktop-outlined />
                     <span>Option 2</span>
-                </a-menu-item>
-                <a-sub-menu key="sub1">
-                    <template #title>
-                        <span>
-                            <user-outlined />
-                            <span>User</span>
-                        </span>
-                    </template>
-                    <a-menu-item key="3">Tom</a-menu-item>
-                    <a-menu-item key="4">Bill</a-menu-item>
-                    <a-menu-item key="5">Alex</a-menu-item>
-                </a-sub-menu>
-                <a-sub-menu key="sub2">
-                    <template #title>
-                        <span>
-                            <team-outlined />
-                            <span>Team</span>
-                        </span>
-                    </template>
-                    <a-menu-item key="6">Team 1</a-menu-item>
-                    <a-menu-item key="8">Team 2</a-menu-item>
-                </a-sub-menu>
-                <a-menu-item key="9">
-                    <file-outlined />
-                    <span>File</span>
-                </a-menu-item>
-            </a-menu>
-        </a-layout-sider>
+                </a-menu-item> -->
+      <a-sub-menu key="sub1">
+        <template #title>
+          <span>
+            <user-outlined />
+            <span>Quản lí người dùng</span>
+          </span>
+        </template>
+        <a-menu-item key="3">Tạo người dùng</a-menu-item>
+        <a-menu-item key="4">Tất cả người dùng</a-menu-item>
+        <a-menu-item key="5">Tài khoản chờ duyệt</a-menu-item>
+        <a-menu-item key="6">Thêm mã giảm giá cho người dùng</a-menu-item>
+        <a-menu-item key="7">Thống kê người giới thiệu</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub2">
+        <template #title>
+          <span>
+            <team-outlined />
+            <span>Quản lí sản phẩm</span>
+          </span>
+        </template>
+        <a-menu-item key="8">Thêm sản phẩm</a-menu-item>
+        <a-menu-item key="9">Tất cả sản phẩm</a-menu-item>
+        <a-menu-item key="10">Quản lí đánh giá</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub3">
+        <template #title>
+          <span>
+            <team-outlined />
+            <span>Quản lí đơn hàng</span>
+          </span>
+        </template>
+        <a-menu-item key="11">Thêm đơn hàng</a-menu-item>
+        <a-menu-item key="12">Tất cả đơn hàng</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub4">
+        <template #title>
+          <span>
+            <team-outlined />
+            <span>Quản lí mã giảm giá</span>
+          </span>
+        </template>
+        <a-menu-item key="13">Thêm mã giảm giá</a-menu-item>
+        <a-menu-item key="14">Tất cả mã giảm giá</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub5">
+        <template #title>
+          <span>
+            <team-outlined />
+            <span>Quản lí bài viết</span>
+          </span>
+        </template>
+        <a-menu-item key="13">Thêm bài viết</a-menu-item>
+        <a-menu-item key="14">Tất cả bài viết</a-menu-item>
+        <a-menu-item key="15">Quản lí bình luận</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub6">
+        <template #title>
+          <span>
+            <team-outlined />
+            <span>Quản lí tủ sách</span>
+          </span>
+        </template>
+        <a-menu-item key="16">Thêm tủ sách</a-menu-item>
+        <a-menu-item key="17">Tất cả tủ sách</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub7">
+        <template #title>
+          <span>
+            <team-outlined />
+            <span>Quản lí kệ sách</span>
+          </span>
+        </template>
+        <a-menu-item key="18">Thêm kệ sách</a-menu-item>
+        <a-menu-item key="19">Tất cả kệ sách</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub8">
+        <template #title>
+          <span NuxtLin>
+            <team-outlined />
+            <span>Quản lí danh mục</span>
+          </span>
+        </template>
+        <NuxtLink to="/admin/category/product">
+          <a-menu-item key="20">Danh mục sản phẩm</a-menu-item>
+        </NuxtLink>
+        <NuxtLink to="/admin/category/post">
+          <a-menu-item key="21">Danh mục bài viết</a-menu-item>
+        </NuxtLink>
+      </a-sub-menu>
+      <a-sub-menu key="sub9">
+        <template #title>
+          <span>
+            <team-outlined />
+            <span>Quản lí tác giả</span>
+          </span>
+        </template>
+        <a-menu-item key="22">Thêm tác giả</a-menu-item>
+        <a-menu-item key="23">Tất cả tác giả</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub10">
+        <template #title>
+          <span>
+            <team-outlined />
+            <span>Quản lí nhà xuất bản</span>
+          </span>
+        </template>
+        <a-menu-item key="22">Thêm nhà xuất bản</a-menu-item>
+        <a-menu-item key="23">Tất cả nhà xuất bản</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub11">
+        <template #title>
+          <span>
+            <team-outlined />
+            <span>Quản lí hóa đơn nhập hàng</span>
+          </span>
+        </template>
+        <a-menu-item key="22">Thêm hóa đơn</a-menu-item>
+        <a-menu-item key="23">Tất cả hóa đơn</a-menu-item>
+      </a-sub-menu>
+    </a-menu>
+  </a-layout-sider>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 const props = defineProps<{
-    collapsed: boolean;
+  collapsed: boolean;
 }>();
 const selectedKeys = ref<string[]>(["1"]);
 </script>
--- pages/admin/category/post.vue
+++ pages/admin/category/post.vue
@@ -1,17 +1,123 @@
 <template>
-    <div>
-        <div
-            class="flex flex-col gap-2 py-4 md:flex-row md:items-center print:hidden"
-        >
-            <div class="grow">
-                <h5 class="text-xl text-[#1e293b] font-semibold">List View</h5>
-            </div>
-            <CommonBreadcrumAdmin />
-        </div>
-  
-        <!-- Đây là phần code mẫu body -->
-        <div class="bg-white min-h-[360px] w-full rounded-lg p-5">
-          Nội dung
-        </div>
+  <div>
+    <div
+      class="flex flex-col gap-2 py-4 md:flex-row md:items-center print:hidden"
+    >
+      <div class="grow">
+        <h5 class="text-xl text-[#1e293b] font-semibold">Danh mục bài viết</h5>
+      </div>
+      <CommonBreadcrumAdmin />
     </div>
-  </template>
+
+    <div class="bg-white min-h-[260px] w-full rounded-lg p-5">
+      <div class="flex justify-between">
+        <div class="relative w-1/4 md:block hidden">
+          <div class="flex">
+            <input
+              type="text"
+              class="w-full border border-gray-300 rounded-md py-2 px-4 pl-10 focus:outline-none focus:border-blue-500"
+              placeholder="Tìm kiếm..."
+            />
+          </div>
+          <div
+            class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+          >
+            <SearchOutlined class="text-gray-500" />
+          </div>
+        </div>
+        <div class="">
+          <a-button type="primary" @click="showModal"
+            >Thêm danh mục bài viết</a-button
+          >
+          <a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </a-modal>
+        </div>
+      </div>
+      <a-table :columns="columns" :data-source="data">
+        <template #bodyCell="{ column, text }">
+          <template v-if="column.dataIndex === 'name'">
+            <a>{{ text }}</a>
+          </template>
+        </template>
+      </a-table>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+    width: 80,
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address 1',
+    ellipsis: true,
+  },
+  {
+    title: 'Long Column ',
+    dataIndex: 'address',
+    key: 'address 2',
+    ellipsis: true,
+  },
+  {
+    title: 'Long Column ',
+    dataIndex: 'address',
+    key: 'address 3',
+    ellipsis: true,
+  },
+  {
+    title: 'Long Column',
+    dataIndex: 'address',
+    key: 'address 4',
+    ellipsis: true,
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park,',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 2 Lake Park,',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park,',
+    tags: ['cool', 'teacher'],
+  },
+];
+const open = ref<boolean>(false);
+
+const showModal = () => {
+  open.value = true;
+};
+
+const handleOk = (e: MouseEvent) => {
+  console.log(e);
+  open.value = false;
+};
+
+</script>
