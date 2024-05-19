a575017dde2e9e3d35d46eb926f857134655a069
Tên của bạn <email@ca.bn>
2024-05-19 13:39:02 +0700

    renderproducts

--- components/Common/Book.vue
+++ components/Common/Book.vue
@@ -4,12 +4,12 @@
             <img class="rounded-lg w-40 h-56" src="https://m.media-amazon.com/images/I/51fsx1MH+tL.jpg" alt="">
         </div>
         <div class="flex flex-col gap-3">
-            <div class="text-xl font-bold hover:text-[#f65d4e]">{{ dataBooks.title }}</div>
+            <div class="text-xl font-bold hover:text-[#f65d4e]">Đây là quyển sách nha</div>
             <div class=" flex justify-start">
                 <CommonStar rating="5" />
             </div>
             <div class="text-sm text-[#999999] hover:text-[#f65d4e]">
-                {{ dataBooks.author.author }}
+                Nguyễn Nhật Ánh
             </div>
             <div class="text-[#f65d4e] font-extrabold text-xl">
                 200.000đ
