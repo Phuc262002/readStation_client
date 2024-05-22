f7d4293e1c02a88b8ffa7a41464c62b6a149d4ac
Nguyen Hoang Phuc <81422862+Phuc262002@users.noreply.github.com>
2024-05-22 21:03:35 +0700

    chore: Update cssnano configuration in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,10 @@ export default defineNuxtConfig({
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
-      cssnano: process.env.NODE_ENV === 'production' ? { preset: ['default', { discardComments: { removeAll: true } }] } : false,
+      cssnano:
+        process.env.NODE_ENV === 'production'
+          ? { preset: ['default', { discardComments: { removeAll: true } }] }
+          : false, // disable cssnano when not in production
     },
   },
   runtimeConfig: {
