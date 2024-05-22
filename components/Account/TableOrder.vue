796cecdb40cccf59360fe053d0d004e070d144ab
Trung <trungtqps26857@fpt.edu.vn>
2024-05-22 20:23:20 +0700

    up

--- components/Account/TableOrder.vue
+++ components/Account/TableOrder.vue
@@ -1,11 +1,14 @@
 <template>
-  <a-table :columns="columns" :data-source="data" :pagination="pagination" bordered >
+  <a-table
+    :columns="columns"
+    :data-source="data"
+    :pagination="pagination"
+    bordered
+  >
     <!-- <a-table :columns="columns" :data-source="data" :pagination="pagination" bordered > -->
     <template #headerCell="{ column }">
       <template v-if="column.key === 'name'">
-        <span>
-          Mã đơn hàng
-        </span>
+        <span> Mã đơn hàng </span>
       </template>
     </template>
 
@@ -20,7 +23,13 @@
           <a-tag
             v-for="tag in record.tags"
             :key="tag"
-            :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+            :color="
+              tag === 'loser'
+                ? 'volcano'
+                : tag.length > 5
+                ? 'geekblue'
+                : 'green'
+            "
           >
             {{ tag.toUpperCase() }}
           </a-tag>
@@ -47,73 +56,71 @@
 <script lang="ts" setup>
 const columns = [
   {
-    title: 'Mã đơn hàng',
-    dataIndex: 'order_id',
-    key: 'order_id',
+    title: "Mã đơn hàng",
+    dataIndex: "order_id",
+    key: "order_id",
     width: 120,
   },
   {
-    title: 'Ngày đặt',
-    dataIndex: 'book_date',
-    key: 'book_date',
+    title: "Ngày đặt",
+    dataIndex: "book_date",
+    key: "book_date",
   },
   {
-    title: 'Ngày nhận',
-    dataIndex: 'book_receipt_date',
-    key: 'book_receipt_date',
+    title: "Ngày nhận",
+    dataIndex: "book_receipt_date",
+    key: "book_receipt_date",
   },
   {
-    title: 'Thời gian trả sách',
-    key: 'book_return',
-    dataIndex: 'book_return',
+    title: "Thời gian trả sách",
+    key: "book_return",
+    dataIndex: "book_return",
   },
   {
-    title: 'Phương thức thanh toán',
-    key: 'payment_methods',
-    dataIndex: 'payment_methods',
+    title: "Phương thức thanh toán",
+    key: "payment_methods",
+    dataIndex: "payment_methods",
   },
   {
-    title: 'Trạng thái đơn',
-    key: 'status',
-    dataIndex: 'status',
+    title: "Trạng thái đơn",
+    key: "status",
+    dataIndex: "status",
   },
   {
-    title: 'Tổng tiền',
-    key: 'total',
-    dataIndex: 'total',
+    title: "Tổng tiền",
+    key: "total",
+    dataIndex: "total",
   },
   {
-    title: 'Gia hạn',
-    key: 'extend',
-    dataIndex: 'extend',
+    title: "Gia hạn",
+    key: "extend",
+    dataIndex: "extend",
   },
   {
-    title: 'Action',
-    key: 'action',
+    title: "Action",
+    key: "action",
   },
 ];
 
 const data = [
   {
-    key: '1',
-    order_id: '2683242197997',
-    book_date: '20/5/2024',
-    book_receipt_date: '22/5/2024',
-    book_return: '22/8/2024',
-    payment_methods: 'credit',
-    status: 'Hoàn thành',
-    total:'200k',
-    extend:'thời gian',
-    tags: ['nice', 'developer'],
+    key: "1",
+    order_id: "2683242197997",
+    book_date: "20/5/2024",
+    book_receipt_date: "22/5/2024",
+    book_return: "22/8/2024",
+    payment_methods: "credit",
+    status: "Hoàn thành",
+    total: "200k",
+    extend: "thời gian",
+    tags: ["nice", "developer"],
   },
 ];
 
-
 const pagination = {
-  total: data.length, 
+  total: data.length,
   pageSize: 2,
   // showTotal: (total, range) => `Đang hiển thị ${range[1] - range[0] + 1} trên ${total} sản phẩm`,
   // position: ['bottomCenter'],
 };
 </script>
-
