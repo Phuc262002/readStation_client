424468bd92e52abc1e3f389ace8498e91d680950
tuitenkiet <tuannkiet9999@gmail.com>
2024-05-24 20:54:44 +0700

    fix featureAuthor

--- components/Home/FeatureAuthor.vue
+++ components/Home/FeatureAuthor.vue
@@ -17,7 +17,7 @@
             </div>
             <div class="w-1/3 flex flex-col gap-2">
                 <div class="grid grid-cols-2 gap-4" v-if="AuthorFeatured.books && AuthorFeatured.books.length > 0">
-                    <CommonBook  v-for="(books,index) in AuthorFeatured.books" :data=books :key="index" />  
+                    <!-- <CommonBook  v-for="(books,index) in AuthorFeatured.books" :data=books :key="index" />   -->
                 </div>
             </div>
         </div>
@@ -25,20 +25,20 @@
 </template>
 
 <script setup>
-const isLoading = ref(false);
-const bookstore = useBookStore();
-const AuthorFeatured = ref({});
+// const isLoading = ref(false);
+// const bookstore = useBookStore();s
+// const AuthorFeatured = ref({});
 
 
-useAsyncData(async () => {
-    isLoading.value = true;
-    try {
-        const response = await bookstore.getAuthorFeatured(); //promise
-        AuthorFeatured.value = response.data._rawValue?.data[0];
-    } catch (error) {
-        console.error(error);
-    } finally {
-        isLoading.value = false;
-    }
-});
+// useAsyncData(async () => {
+//     isLoading.value = true;
+//     try {
+//         const response = await bookstore.getAuthorFeatured(); //promise
+//         AuthorFeatured.value = response.data._rawValue?.data[0];
+//     } catch (error) {
+//         console.error(error);
+//     } finally {
+//         isLoading.value = false;
+//     }
+// });
 </script>
