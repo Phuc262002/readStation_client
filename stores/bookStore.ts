78ce49f9b14bc5e193ffff48270bf9b31ac3c2c9
tuitenkiet <tuannkiet9999@gmail.com>
2024-06-02 16:50:55 +0700

    Show AllBooks Admin

--- stores/bookStore.ts
+++ stores/bookStore.ts
@@ -1,15 +1,18 @@
 import create from "@ant-design/icons-vue/lib/components/IconFont";
-import { defineStore } from "pinia"; 
+import { defineStore } from "pinia";
 
 export const useBookStore = defineStore("book-store", {
   state: () => {
     return {
       books: [],
+      adminBooks: [],
     };
   },
   actions: {
     async getAuthorFeatured() {
-      const data: any = await useCustomFetch("/api/v1/home/get-feautured-author");
+      const data: any = await useCustomFetch(
+        "/api/v1/home/get-feautured-author"
+      );
       return data;
     },
     async getAllBooks() {
@@ -17,12 +20,23 @@ export const useBookStore = defineStore("book-store", {
       return data;
     },
     async createBook(valuecreateBook: any) {
-      const data : any = await useCustomFetch("/api/v1/books/create-full",{
+      const data: any = await useCustomFetch("/api/v1/books/create-full", {
         method: "POST",
         body: JSON.stringify(valuecreateBook),
-      })
+      });
       return data;
-    }
+    },
+    async getAdminBooks({
+      page,
+      pageSize,
+      search,
+      category_id,
+      author_id,
+      status,
+    }: any) {
+      const data: any = await useCustomFetch("/api/v1/books/admin/get-all");
+      this.adminBooks = data.data._value?.data;
+      return data;
+    },
   },
-
 });
