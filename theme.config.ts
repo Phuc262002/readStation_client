26e8cd1bafff684bb922e4f7c838e71d2bafb73f
Nguyen Hoang Phuc <81422862+Phuc262002@users.noreply.github.com>
2024-06-03 17:37:58 +0700

    refactor: Update app.vue layout and theme configuration

---
+++ theme.config.ts
@@ -0,0 +1,39 @@
+const themeAntdConfig = Object.freeze({
+    // apply ant design theme
+    token: {
+        // primary color
+        colorPrimary: "#FF7D29",
+        colorLink: "blue",
+        // hover color
+        colorPrimaryHover: "#fe6411",
+        colorLinkActive: "#c63408",
+        colorLinkHover: "#fe6411",
+        // wireframe: false,
+        // fontSize: 14,
+        // borderRadius: 4,
+        // sizeStep: 4,
+        fontFamily: `'Nunito', sans-serif`,
+        // lineHeight: 1.5714285714285714,
+        // colorError: "#ED0131"
+    },
+    components: {
+        // Radio: {
+        //     // orange color
+        //     colorPrimary: "orange"
+        // },
+        // Input: {
+        //     controlHeight: 48,
+        //     borderRadius: 4
+        // },
+        // Button: {
+        //     controlHeight: 60,
+        //     borderRadius: 10
+        // },
+        // Select: {
+        //     controlHeight: 48,
+        //     borderRadius: 4
+        // }
+    }
+});
+
+export default themeAntdConfig;
