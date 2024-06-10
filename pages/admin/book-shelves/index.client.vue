70aed7e15dfc1f8fd7bca3c17909e87726088b7e
tuitenkiet <tuannkiet9999@gmail.com>
2024-06-10 18:38:43 +0700

    detailShelves

--- pages/admin/book-shelves/index.client.vue
+++ pages/admin/book-shelves/index.client.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <div
-      class="flex flex-col gap-2 py-4 md:flex-row md:items-center print:hidden"
-    >
+    <div class="flex flex-col gap-2 py-4 md:flex-row md:items-center print:hidden">
       <div class="grow">
         <h5 class="text-xl text-[#1e293b] font-semibold">Tất cả kệ sách</h5>
       </div>
@@ -17,90 +15,84 @@
                 <SearchOutlined />
               </template>
               <template #suffix>
-                <a-spin  :indicator="indicator" />
+                <a-spin :indicator="indicator" />
               </template>
             </a-input>
           </div>
-          <div
-            class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
-          >
+          <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
             <UIcon class="text-gray-500" name="i-material-symbols-search" />
           </div>
         </div>
         <div class="">
           <a-button type="primary" @click="showModalAdd">Thêm kệ sách</a-button>
-          <BookShelvesCreate
-            :openModalAdd="openModalAdd"
-            :openModal="CloseModalAdd"
-          />
-          <BookShelvesEdit
-            :openModalEdit="openModalEdit"
-            :openModal="CloseModalEdit"
-            :shelvesId="shelvesId"
-          />
+          <BookShelvesCreate :openModalAdd="openModalAdd" :openModal="CloseModalAdd" />
+          <BookShelvesEdit :openModalEdit="openModalEdit" :openModal="CloseModalEdit" :shelvesId="shelvesId" />
         </div>
       </div>
 
-      <a-table
-        :columns="columns"
-        :data-source="dataShelves"
-        :loading="shelvesValue.isLoading"
-      >
-        <template #headerCell="{ column }">
-          <template v-if="column.key === 'name'">
-            <span> Mã tủ sách </span>
-          </template>
-        </template>
-
+      <a-table :columns="columns" :data-source="dataShelves" :loading="shelvesValue.isLoading">
         <template #bodyCell="{ column, record, index }">
-          <template v-if="column.key === '#'">
-            <a>
-              {{ index + 1 }}
+          <template v-if="column.key === 'bookshelf_code'">
+            <a class="text-[#3B82F6]">
+              {{ record.bookshelf_code }}
             </a>
           </template>
           <template v-if="column.key === 'bookcase_id'">
-            <span>
+            <a class="text-[#3B82F6]">
               {{ record.bookcase.description }}
-            </span>
+            </a>
           </template>
           <template v-if="column.key === 'category_id'">
             <span>
               {{ record.category.name }}
             </span>
           </template>
+          <template v-if="column.key === 'shelves'">
+            <span class="flex justify-start gap-2">
+              <p>kệ sách</p>
+            </span>
+          </template>
           <template v-if="column.key === 'books'">
+            <span class="flex justify-start gap-2">
+              {{ record.books.length }} <p>cuốn sách</p>
+            </span>
+          </template>
+          <template v-if="column.key === 'status'">
             <span>
-              {{ record.books.length }}
+              <a-tag :color="record.status === 'active' ? 'green' : 'volcano'">
+                {{ record.status }}
+              </a-tag>
             </span>
           </template>
           <template v-else-if="column.key === 'action'">
             <div class="flex text-[16px] gap-4">
+              <NuxtLink :to="`book-shelves/detail-shelves/${record.id}`">
+                <a-tooltip placement="top" color="yellow">
+                  <template #title>
+                    <span>Xem chi tiết</span>
+                  </template>
+                  <button
+                    class="group hover:bg-[#212122]/20 bg-[#e4e1e1] flex items-center justify-center w-8 h-8 rounded-md">
+                    <UIcon class="group-hover:text-[#212122]" name="i-icon-park-outline-eyes" />
+                  </button>
+                </a-tooltip>
+              </NuxtLink>
               <a-tooltip placement="top" color="green">
                 <template #title>
                   <span>Sửa</span>
                 </template>
-                <button
-                  @click="showModalEdit(record?.id)"
-                  class="group hover:bg-[#212122]/20 bg-[#e4e1e1] flex items-center justify-center w-8 h-8 rounded-md"
-                >
-                  <UIcon
-                    class="group-hover:text-[#212122]"
-                    name="i-material-symbols-edit-outline"
-                  />
+                <button @click="showModalEdit(record?.id)"
+                  class="group hover:bg-[#212122]/20 bg-[#e4e1e1] flex items-center justify-center w-8 h-8 rounded-md">
+                  <UIcon class="group-hover:text-[#212122]" name="i-material-symbols-edit-outline" />
                 </button>
               </a-tooltip>
               <a-tooltip placement="top" color="red">
                 <template #title>
                   <span>Xóa</span>
                 </template>
-                <button
-                  @click="showDeleteConfirm(record?.id)"
-                  class="group hover:bg-[#212122]/20 bg-[#e4e1e1] flex items-center justify-center w-8 h-8 rounded-md"
-                >
-                  <UIcon
-                    class="group-hover:text-[#212122]"
-                    name="i-material-symbols-delete-outline"
-                  />
+                <button @click="showDeleteConfirm(record?.id)"
+                  class="group hover:bg-[#212122]/20 bg-[#e4e1e1] flex items-center justify-center w-8 h-8 rounded-md">
+                  <UIcon class="group-hover:text-[#212122]" name="i-material-symbols-delete-outline" />
                 </button>
               </a-tooltip>
             </div>
@@ -174,34 +166,39 @@ const showModalEdit = (id: number) => {
 };
 const columns = [
   {
-    title: "#",
-    dataIndex: "#",
-    key: "#",
-  },
-  {
-    title: "Kệ sách",
-    dataIndex: "description",
-    key: "description",
-  },
-  {
-    title: "Mã kệ sách",
+    title: "Mã kệ",
     dataIndex: "bookshelf_code",
     key: "bookshelf_code",
   },
   {
-    title: "Mã tủ sách",
+    title: "Tên tủ",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Thuộc tủ",
     dataIndex: "bookcase",
     key: "bookcase_id",
   },
   {
-    title: "Mã danh mục",
-    dataIndex: "category",
-    key: "category_id",
+    title: "Số lượng kệ",
+    dataIndex: "shelves",
+    key: "shelves",
   },
   {
     title: "Số lượng sách",
     dataIndex: "books",
     key: "books",
+  },
+  {
+    title: "Mô tả",
+    dataIndex: "description",
+    key: "description",
+  },
+  {
+    title: "Trạng thái",
+    dataIndex: "status",
+    key: "status",
   },
   {
     title: "Action",
