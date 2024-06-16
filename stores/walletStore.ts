e0e9f5eba5bddd7cfc7885545c74e8b4447dacbc
Trần Quang Trung <trung.tranquangggg@gmail.com>
2024-06-16 16:49:41 +0700

    update payment transaction

--- stores/walletStore.ts
+++ stores/walletStore.ts
@@ -2,6 +2,7 @@ import { defineStore } from "pinia";
 export const useWalletStore = defineStore("wallet-store", {
   state() {
     return {
+      paymentLink: "",
       wallets: [],
       transactions: [],
       isLoading: false,
@@ -10,13 +11,41 @@ export const useWalletStore = defineStore("wallet-store", {
   },
   persist: true,
   actions: {
-    async getTransaction({ page, pageSize, sort }: any) {
+    async getAllTransaction({ page, pageSize, sort }: any) {
       const data: any = await useCustomFetch(
         `/api/v1/account/wallet/transaction-history?${
           page ? `&page=${page}` : ""
+        }${pageSize ? `&pageSize=${pageSize}` : ""}${
+          sort ? `&sort=${sort}` : ""
         }`
       );
       this.transactions = data.data._value?.data;
+      return data;
+    },
+    async createTransaction(body: any) {
+      const data: any = await useCustomFetch(
+        "/api/v1/account/wallet/create-transaction",
+        {
+          method: "POST",
+          body: body,
+        }
+      );
+      return data;
+    },
+    async getPaymentLink(id) {
+      const data: any = await useCustomFetch(
+        `/api/v1/account/wallet/get-payment-link/${id}`
+      );
+      this.paymentLink = data.data._value?.data;
+      return data;
+    },
+    async updateTransactionStatus({ id }: any) {
+      const data: any = await useCustomFetch(
+        `/api/v1/account/wallet/update-transaction-status/{id}?${
+          id ? `&id=${id}` : ""
+        }`
+      );
+      this.wallets = data.data._value?.data;
       return data;
     },
   },
