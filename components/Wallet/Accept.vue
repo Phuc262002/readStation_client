1d6a0378dfde24b972fba60497d62620e1b22169
khuong03 <khuongttaps26697@fpt.edu.vn>
2024-06-16 16:48:58 +0700

    Update category name field in post edit page

---
+++ components/Wallet/Accept.vue
@@ -0,0 +1,102 @@
+<template>
+    <a-modal
+      v-model:open="props.openModalAccept"
+      title="Xác thực ví"
+      :footer="null"
+      :onCancel="handleClose"
+    >
+      <form @submit.prevent="onSubmit">
+        <div class="bg-white py-2">
+          <div class="flex gap-2">
+            <div class="pb-4">
+              <label for="email" class="block text-sm font-medium text-gray-700">
+                Họ và tên
+              </label>
+              <div class="mt-1">
+                <a-input
+                  class="w-[232px] h-10"
+                  placeholder="Nhập họ và tên"
+                  required
+                />
+              </div>
+            </div>
+            <div class="pb-4">
+              <label for="email" class="block text-sm font-medium text-gray-700">
+                Số điện thoại
+              </label>
+              <div class="mt-1">
+                <a-input
+                  class="w-[232px] h-10"
+                  placeholder="Nhập số điện thoại"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div class="pb-4">
+            <label for="email" class="block text-sm font-medium text-gray-700">
+              CMT/CCCD
+            </label>
+            <div class="mt-1">
+              <a-input
+                class="w-[232px] h-10"
+                placeholder="Nhập số CMT/CCCD"
+                required
+              />
+            </div>
+          </div>
+  
+          <div class="pb-4">
+            <label for="email" class="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <div class="mt-1">
+              <a-input class="w-full h-10" placeholder="Nhập email" required />
+            </div>
+          </div>
+  
+          <div class="pb-4">
+            <label for="email" class="block text-sm font-medium text-gray-700">
+              Số tiền nạp
+            </label>
+            <div class="mt-1">
+              <a-input
+                class="w-[472px] h-[84px] text-3xl font-semibold"
+                placeholder="Nhập số tiền nạp"
+                required
+              />
+            </div>
+          </div>
+  
+          <div class="flex justify-end items-end gap-2">
+            <a-button @click="handleClose" danger html-type="button" class="mt-4"
+              >Hủy</a-button
+            >
+            <a-button type="primary" html-type="submit" class="mt-4"
+              >Nạp tiền</a-button
+            >
+          </div>
+        </div>
+      </form>
+    </a-modal>
+  </template>
+  <script setup>
+  import { message, Upload } from "ant-design-vue";
+  
+  const props = defineProps({
+    openModalAccept: Boolean,
+    openModal: Function,
+  });
+  const open = ref(props.openModalAccept);
+  
+  watch(
+    () => props.openModalAccept,
+    (newVal) => {
+      open.value = newVal;
+    }
+  );
+  const handleClose = () => {
+    props.openModal();
+  };
+  </script>
+  
