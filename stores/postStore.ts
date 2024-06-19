4b08e959b9ecc2554337f1039a65ef0a503e91f3
khuong03 <khuongttaps26697@fpt.edu.vn>
2024-06-19 23:10:56 +0700

    Update image sources and styles in SidebarAdmin.vue, BlogComment.vue, and BlogBanner.vue

--- stores/postStore.ts
+++ stores/postStore.ts
@@ -16,40 +16,49 @@ export const usePostStore = defineStore("post-store", {
       try {
         this.isLoading = true;
         const data: any = await useCustomFetch(
-          `/api/v1/posts?${
-            page ? `&page=${page}` : ""
-          }${pageSize ? `&pageSize=${pageSize}` : ""}${
-            category_id ? `&category_id=${category_id}` : ""
-          }${sort ? `&sort=${sort}` : ""
+          `/api/v1/posts?${page ? `&page=${page}` : ""}${
+            pageSize ? `&pageSize=${pageSize}` : ""
+          }${category_id ? `&category_id=${category_id}` : ""}${
+            sort ? `&sort=${sort}` : ""
           }`
         );
         // this.posts = data.data._value?.data;
         this.posts = data.data._value;
+
         return data;
       } catch (error) {
         console.log(error);
+      } finally {
+        this.isLoading = false;
       }
     },
-    async getAllPost({ page, pageSize, search, category_id, status, type }: any) {
+    async getAllPost({
+      page,
+      pageSize,
+      search,
+      category_id,
+      status,
+      type,
+    }: any) {
       try {
         this.isLoading = true;
         const data: any = await useCustomFetch(
-          `/api/v1/posts/admin/get-all?${
-            page ? `&page=${page}` : ""
-          }${pageSize ? `&pageSize=${pageSize}` : ""}${
-            search ? `&search=${search}` : ""
-          }${category_id ? `&category_id=${category_id}` : ""}${
-            status ? `&status=${status}` : ""
-          }${type ? `&type=${type}` : ""}`
+          `/api/v1/posts/admin/get-all?${page ? `&page=${page}` : ""}${
+            pageSize ? `&pageSize=${pageSize}` : ""
+          }${search ? `&search=${search}` : ""}${
+            category_id ? `&category_id=${category_id}` : ""
+          }${status ? `&status=${status}` : ""}${type ? `&type=${type}` : ""}`
         );
         this.postsAdmin = data.data._value?.data;
-        this.isLoading = false;
+
         return data;
       } catch (error) {
         console.log(error);
+      } finally {
+        this.isLoading = false;
       }
     },
-    async getOnePost(slug: string) { 
+    async getOnePost(slug: string) {
       try {
         this.isLoading = true;
         const data: any = await useCustomFetch(`/api/v1/posts/get-one/${slug}`);
@@ -60,7 +69,7 @@ export const usePostStore = defineStore("post-store", {
         console.log(error);
       }
     },
-    async getOnePostAdmin(id: string) { 
+    async getOnePostAdmin(id: string) {
       try {
         this.isLoading = true;
         const data: any = await useCustomFetch(`/api/v1/posts/get/${id}`);
@@ -71,7 +80,7 @@ export const usePostStore = defineStore("post-store", {
         console.log(error);
       }
     },
-    
+
     async createPost(post: any) {
       try {
         this.isSubmitting = true;
@@ -110,6 +119,5 @@ export const usePostStore = defineStore("post-store", {
         console.log(error);
       }
     },
-    
   },
 });
