373c98fe7d003963f240c132d903a4678fe77b25
khuong03 <khuongttaps26697@fpt.edu.vn>
2024-06-22 12:44:12 +0700

    refactor: Update sidebar menu labels for user management

---
+++ components/Dashboard/Book.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="flex justify-between cursor-pointer hover:bg-[#f1f1f1] p-2 hover:rounded-md"
+  >
+    <div class="flex justify-center items-start">
+      <img
+        class="w-20 h-20 object-cover rounded-md"
+        src="https://cdn0.fahasa.com/media/catalog/product/8/9/8935244875805.jpg"
+        alt=""
+      />
+      <div class=" ">
+        <p>{{ props?.dashboard?.book?.title }}</p>
+        <p>
+          {{ props?.dashboard?.book_version }} -
+          <span> {{ props?.dashboard?.book?.author?.author }}</span>
+        </p>
+        <p class="flex items-center">
+          <span>4.8</span> <span><IconYellowStar /></span>
+        </p>
+      </div>
+    </div>
+    <p class="flex items-start">200 lượt thuê</p>
+  </div>
+  <hr class="my-2" />
+</template>
+<script setup>
+const props = defineProps(["dashboard"]);
+</script>
---
+++ stores/admin/dashboardStore.ts
@@ -0,0 +1,60 @@
+import { defineStore } from "pinia";
+
+export const useDashboardStore = defineStore("dashboard-store", {
+  state: () => {
+    return {
+      dashboardAdmin: [],
+      dashboardBook: [],
+      dashboardInvoice: [],
+      isLoading: false,
+      isSubmitting: false,
+    };
+  },
+  actions: {
+    async getAllDashboard() {
+      try {
+        this.isLoading = true;
+        const data: any = await useCustomFetch(
+          `/api/v1/admin/dashboard/statistic-admin`
+        );
+        this.dashboardAdmin = data.data._value?.data;
+
+        return data;
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    async getBookDashboard() {
+      try {
+        this.isLoading = true;
+        const data: any = await useCustomFetch(
+          `/api/v1/admin/dashboard/book-hire-top-by-month`
+        );
+        this.dashboardBook = data.data._value?.data;
+
+        return data;
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    async getInvoiceDashboard() {
+      try {
+        this.isLoading = true;
+        const data: any = await useCustomFetch(
+          `/api/v1/admin/dashboard/invoice-enter-by-month`
+        );
+        this.dashboardInvoice = data.data._value?.data;
+
+        return data;
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
+});
