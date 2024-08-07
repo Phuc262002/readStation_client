0e0b91737bee4abce9622042f5150137248b9bdb
khuong03 <khuongttaps26697@fpt.edu.vn>
2024-08-07 20:26:11 +0700

    refactor: Update lazy loading for images in contact page

--- pages/admin/contact/index.client.vue
+++ pages/admin/contact/index.client.vue
@@ -12,9 +12,30 @@
 
     <!-- Đây là phần code mẫu body -->
     <div class="bg-white min-h-[360px] w-full rounded-lg p-5">
-      <iframe class="w-full h-[500px]"
+      <a-button
+        size="large"
+        class="float-right flex items-center gap-1"
+        type="primary"
+        @click="
+          () =>
+            navigateTo(
+              'https://docs.google.com/spreadsheets/d/11SLmEnRGgS5R5Q5MyrHD6cINMlir7ihkVrqw5ZBkdkw/edit?usp=sharing',
+              { open: '_blank' }
+            )
+        "
+        ><span>Xem file</span>
+        <Icon
+          icon="heroicons-outline:external-link"
+          width="1.2rem"
+          height="1.2rem"
+      /></a-button>
+      <iframe
+        class="w-full h-[500px]"
         src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR8GXFT0LJe1mvNbP2E-5PxjJ7rtkrKvnD_REUeF2nEAv1N0mkF3F8IN61gtbUjaarpkjJG1V0ns10a/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
       ></iframe>
     </div>
   </div>
 </template>
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
