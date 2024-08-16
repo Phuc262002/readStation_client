60992a9a8904ce7bae431eb12568210f034436d0
khuong03 <khuongttaps26697@fpt.edu.vn>
2024-08-16 22:02:30 +0700

    update

--- pages/admin/book-case/index.client.vue
+++ pages/admin/book-case/index.client.vue
@@ -241,7 +241,6 @@ watch(valueSearch, onSearch);
 useAsyncData(
   async () => {
     await bookCaseStore.getAllBookcases({
-      pageSize: 1000,
       page: current.value,
       search: valueSearch.value,
       status: queryStatus.value.value,
