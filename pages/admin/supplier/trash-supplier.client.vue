845be3ed4460aebf1c0fbdecfc468cd9dba0919e
tuitenkiet <tuannkiet9999@gmail.com>
2024-08-24 14:26:20 +0700

    fix

--- pages/admin/supplier/trash-supplier.client.vue
+++ pages/admin/supplier/trash-supplier.client.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <div
-      class="flex flex-col gap-2 py-4 md:flex-row md:items-center print:hidden"
-    >
+    <div class="flex flex-col gap-2 py-4 md:flex-row md:items-center print:hidden">
       <div class="grow">
         <h5 class="text-xl text-[#1e293b] font-bold">
           Tất cả nhà cung cấp đã xóa
@@ -12,12 +10,8 @@
 
     <div class="bg-white min-h-[360px] w-full rounded-lg p-5 shadow-sm">
       <div>
-        <a-table
-          :columns="columns"
-          :data-source="supplierStore?.SupplierAdmin?.suppliers"
-          :loading="supplierStore.isLoading"
-          :pagination="false"
-        >
+        <a-table :columns="columns" :data-source="supplierStore?.SupplierAdmin?.suppliers"
+          :loading="supplierStore.isLoading" :pagination="false">
           <template #bodyCell="{ column, record, index }">
             <template v-if="column.key === 'name'">
               <a>
@@ -40,14 +34,9 @@
                   <template #title>
                     <span>Khôi phục</span>
                   </template>
-                  <button
-                    @click="showRecoverConfirm(record?.id)"
-                    class="group hover:bg-[#212122]/20 bg-[#e4e1e1] flex items-center justify-center w-8 h-8 rounded-md"
-                  >
-                    <Icon
-                      icon="ic:round-settings-backup-restore"
-                      class="text-lg"
-                    />
+                  <button @click="showRecoverConfirm(record?.id)"
+                    class="group hover:bg-[#212122]/20 bg-[#e4e1e1] flex items-center justify-center w-8 h-8 rounded-md">
+                    <Icon icon="ic:round-settings-backup-restore" class="text-lg" />
                   </button>
                 </a-tooltip>
               </div>
@@ -56,13 +45,9 @@
         </a-table>
       </div>
       <div class="mt-4 flex justify-end">
-        <a-pagination
-          v-if="supplierStore.getAllSupplier?.totalResults > 0"
-          v-model:current="current"
-          :total="supplierStore.getAllSupplier?.totalResults"
-          :pageSize="supplierStore.getAllSupplier?.pageSize"
-          show-less-items
-        />
+        <a-pagination v-if="supplierStore.getAllSupplier?.totalResults > 0" v-model:current="current"
+          :total="supplierStore.getAllSupplier?.totalResults" :pageSize="supplierStore.getAllSupplier?.pageSize"
+          show-less-items />
       </div>
     </div>
   </div>
@@ -76,6 +61,9 @@ useAsyncData(async () => {
     page: current.value,
     status: "deleted",
   });
+}, {
+  immediate: true,
+  watch: [current],
 });
 const showRecoverConfirm = (id) => {
   Modal.confirm({
